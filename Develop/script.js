a1d67738349fa96a7643ceedf7291ee4e92057e1
ElmerPC <erey2790@gmail.com>
2021-12-08 18:29:17 -0600

    added psuedocode

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,21 @@
 
 function generatePassword() {
   console.log("Button has been clicked!!!")
+
+//prompt the user for the password
+
+//password lenght 8-128
+
+// lowercase, uppercase , number special characters
+
+// ensure the character lenght is correct
+
+// at least one charter lenght is selected input validation
+
+// display the password to the site.
+
+
+  return "Generated password will go here."
 }
 
 // Get references to the #generate element
